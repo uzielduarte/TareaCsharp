388a6b3a8a1117053ee175b08768b62e0428b7f4
uzielduarte <duarteuziel36@gmail.com>
2021-06-19 11:00:42 -0600

    Agrego el programa del profesor al proyecto para trabajar en la tarea

---
+++ Infraestructure/Data/ClientRepository.cs
@@ -0,0 +1,48 @@
+﻿using Core.Interfaces;
+using Core.Poco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Data
+{
+    public class ClientRepository : IClientRepository
+    {
+        private RAFContext context;
+        private readonly int SIZE = 228;
+
+        public ClientRepository()
+        {
+
+        }
+        #region Metodos
+        public void Create(Cliente t)
+        {
+            context.Create<Cliente>(t);
+        }
+
+        public int Update(Cliente t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Cliente t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cliente> GetAll()
+        {
+            return context.GetAll<Cliente>();
+        }
+
+        public IEnumerable<Cliente> Find(Expression<Func<Cliente, bool>> where)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ Presentation/Form1.cs
@@ -0,0 +1,63 @@
+﻿using Core.Poco;
+using Infraestructure.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation
+{
+    public partial class Form1 : Form
+    {
+        private ProductRepository productRepository;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            productRepository = new ProductRepository();
+        }
+
+        private void BtnOk_Click(object sender, EventArgs e)
+        {
+            Product p = new Product()
+            {
+                Name = "Galleta",
+                Brand = "Nabisco",
+                Model = "Chiky",
+                Description = "Galleta de Vainilla",
+                Price = 10M,
+                Stock = 10,
+                ImageURL = ""
+            };
+
+            productRepository.Create(p);
+         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            List<Product> products = productRepository.GetAll().ToList();
+            if(products == null || products.Count == 0)
+            {
+                Console.WriteLine("No data");
+            }
+
+            products.ForEach(p => Console.WriteLine($"Id: {p.Id} Name: {p.Model}"));
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            List<Product> products = productRepository.Find(p => p.Model.Equals("Oreo", StringComparison.InvariantCultureIgnoreCase))
+                                                      .ToList();
+
+            products.ForEach(p => Console.WriteLine($"Nombre: {p.Model}"));
+        } 
+    }
+}
